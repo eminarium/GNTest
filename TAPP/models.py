1fe7ec05fe44bb729e7a4418a894151e15e75179
eminarium <merdan.durdiyev@gmail.com>
2018-11-08 19:57:14 +0500

    Created Seasons Model and migrated

--- TAPP/models.py
+++ TAPP/models.py
@@ -7,11 +7,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
 
     class Meta:
       db_table = "teachers"
 
 
+class Season(models.Model):
+    season_no = models.IntegerField(blank = False)
+    season_title = models.CharField(max_length = 50, blank = False)
+    season_from_date = models.DateField(blank = False)
+    season_to_date = models.DateField(blank = False)
+    notes = models.TextField(blank = True)
 
+    class Meta:
+        db_table = "seasons"
 
