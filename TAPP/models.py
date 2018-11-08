7317382c0d2bbf3f25fd953eb5eab07337f87600
eminarium <merdan.durdiyev@gmail.com>
2018-11-08 19:47:22 +0500

    Created Teacher Model and migrated

--- TAPP/models.py
+++ TAPP/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+      db_table = "teachers"
+
+
+
+
