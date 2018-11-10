5e1fde5cfc01ca99509bfe40e1993ac3d3fc76d2
eminarium <merdan.durdiyev@gmail.com>
2018-11-10 19:16:33 +0500

    Set up static files and first view file (season-index)

--- GNT/settings.py
+++ GNT/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'GNT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = ''
--- TAPP/views.py
+++ TAPP/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView # Import TemplateView
 
 # Create your views here.
+
+class index(TemplateView):
+    template_name = "index.html"
